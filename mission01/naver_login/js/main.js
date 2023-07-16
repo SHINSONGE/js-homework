980f0e18747f65d6c85d0192808b38b9f4bb1e89
SHINSONGE <seul>
2023-07-16 23:51:02 +0900

    미완성본 제출..

--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,58 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
+
+//  첫번째 시도 실패
+/* const inputEmail = document.querySelector("#userEmail");
+const inputPassword = document.getElementById("#userPassword");
+const loginBtn = document.getElementById("#loginBtn");
+
+let value = inputEmail.value;
+
+inputEmail.addEvetListener("inputEmail", () => {
+  value = inputEmail.value;
+  console.log(value === "asd@naver.com");
+}); */
+
+//두번째시도 실패
+/* function checkLogin() {
+  const username = document.getElementById("#userEmail").value; // 아이디 입력란
+  const password = document.getElementById("#userPassword").value; // 비밀번호 입력란
+  const loginBtn = document.getElementById("#loginBtn");
+
+  // 아이디와 비밀번호가 일치하는지 확인
+  if (usernameRegex.test(username) && passwordRegex.test(password)) {
+    // 로그인 성공 시 보여줄 요소 표시
+    // function goUrl() {
+    //   const link = "welcome.html";
+    //   location.href = link;
+    // }
+    loginBtn.addEventListener("click");
+  } else {
+    // 로그인 실패 시 처리할 내용
+    alert("아이디 또는 비밀번호가 잘못되었습니다.");
+  }
+} */
+
+// 세번째 시도 실패각 범쌤 죄송해여..
+const id = document.getElementById("#userEmail").value; // 아이디 입력란
+const password = document.getElementById("#userPassword").value; // 비밀번호 입력란
+const loginBtn = document.getElementById("#loginBtn"); //로그인버튼
+
+id.addEventListener("input", function () {
+  if (emailReg(this.value)) {
+    this.classList.add("is-active");
+  } else {
+    this.classList.remove("is-active");
+  }
+});
+
+GamepadButton.addEventListener("click", () => {
+  if (idpass) {
+    id === emailReg ? (window.location.href = "welcome.html") : false;
+  }
+});
 
 /*
 
@@ -13,24 +63,14 @@ const user = {
 
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+function emailReg(username) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
-}
-
-function pwReg(text){
-  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
-
-
-
-
-
-
-
-
-
-
+function pwReg(password) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
